d0a0a41547f085ea89ec7ff811a130658031b589
dan <dan@example.com>
2021-05-28 21:45:34 -0400

    submission 1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Fairwinds-Tech
+
+Assumptions: 
+
+1. It was decided to use Django as web application framework. 
+
+2. The final deployment is not production ready.  Django is running in DEBUG mode and it has a light weight web server for testing and deveopment purpose.  ALLOWED_HOSTS = ['*'] is configured for easy testing also. 
+
+3.  AMI is "Ubuntu Server 20.04 LTS (HVM), SSD Volume Type"   
+
+4.  It is assumed the client pc is running Ubunto Desktop 20.04.  
+
+Deployment Steps:
+
+1. Copy the credential file to ~/fairwinds-tech and name it as file "cred". 
+
+2. (optional) Run ~/fairwinds-tech/cleanup.sh to clean up.
+
+3. Run ~/fairwinds-tech/fairwinds-deploy.sh Bash script.  This will setup AWS credentials to prepare the execution of deployEC2.py Python script and run it at the end.   
+
+deployEC2.py will deploy EC2 instance and install docker to prepare ec2_django container run on the EC2 instance.  It will download an image from DockerHub called "danielcchen/ec2_django".  The address to connect to the django container will be provided when script exit.
+
+Future Improvement:
+
+It is assume keypair needs to be created each time the script is started. The deployEC2.py should check if the keypair exist and then create keypair if required. 
---
+++ createKey.py
@@ -0,0 +1,88 @@
+import boto3
+import os
+import time
+import subprocess
+import paramiko
+
+
+ec2 = boto3.resource('ec2', region_name='us-west-1')
+
+# create a file to store the key locally
+outfile = open('ec2-keypair.pem','w')
+
+# call the boto ec2 function to create a key pair
+key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+
+# capture the key and store it in a file
+KeyPairOut = str(key_pair.key_material)
+print(KeyPairOut)
+outfile.write(KeyPairOut)
+outfile.close()
+
+os.chmod("ec2-keypair.pem", 0o400)
+
+# Create ec2 and use SubnetId to associate VPC.
+instances = ec2.create_instances(
+     ImageId='ami-0d382e80be7ffdae5',
+     MinCount=1,
+     MaxCount=1,
+     InstanceType='t2.micro',
+     KeyName='ec2-keypair',
+     SubnetId='subnet-00f10fc4d76c44151'
+ )
+
+instance = instances[0]
+
+instance.wait_until_running()
+
+# Associate Security Group to the instance to allow access to instance VPC 
+instance.modify_attribute(Groups=['sg-01db77201ef264fa4'])
+
+# Reload to get the DNS name after EC2 is up and running
+instance.load()
+hostname = instance.public_dns_name
+
+key = paramiko.RSAKey.from_private_key_file('ec2-keypair.pem')
+
+client = paramiko.SSHClient()
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connect/ssh to an instance
+
+try:
+
+    # Here 'ubuntu' is user name and 'hostname' is public DNS name 
+
+    client.connect(hostname=hostname, username="ubuntu", pkey=key)
+
+    # Execute a command(cmd) after connecting/ssh to an instance
+
+    cmds = ["sudo apt update",  
+            "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+            "curl -fsSL https:\/\/download.docker.com\/linux/ubuntu/gpg | sudo apt-key add -",  
+            "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",  
+            "sudo apt update",  
+            "apt-cache policy docker-ce",  
+            "sudo apt install -y docker-ce", 
+            "sudo docker run -d -p 8000:8000 danielcchen\/django_ec2"]
+
+    for cmd in cmds:
+        print(cmd) 
+        stdin,stdout,stderr=client.exec_command(cmd)
+        outlines=stdout.readlines()
+        result=''.join(outlines)
+        print (result)        
+
+    client.close()
+
+
+except Exception as e:
+
+    print(e)
+
+print(hostname)
+print("ssh -i ec2-keypair.pem ubuntu@{}".format(hostname))
+print("Please use this address to access the website http://{}:8000".format(hostname))
+
+
